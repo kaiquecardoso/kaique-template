3cab249ef474eefc2bc0432fc7da68d0db3a9830
Kaique Cardoso <kaiquecardoso@gmail.com>
2025-08-14 00:25:27 -0300

    Remove arquivos não utilizados e dependências obsoletas, incluindo componentes de formulário, testes e ícones. Atualiza o arquivo package.json para refletir as mudanças nas dependências e remove configurações de teste do Vite. Limpa o código para melhorar a manutenção e a legibilidade.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './routes'
 import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
 
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
@@ -14,8 +13,3 @@ if (rootElement && !rootElement.innerHTML) {
     </StrictMode>,
   )
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
